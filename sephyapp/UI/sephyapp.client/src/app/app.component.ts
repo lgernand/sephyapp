93a364772a7e6f2a85826750aca260729c254623
lgernand <landongernand@outlook.com>
2025-11-20 09:22:17 -0600

    removed unused imports

--- sephyapp/UI/sephyapp.client/src/app/app.component.ts
+++ sephyapp/UI/sephyapp.client/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
-import { FormsModule } from '@angular/forms'
 import { LoginComponent } from "./components/login/login.component";
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe, ReactiveFormsModule, LoginComponent],
+  imports: [ReactiveFormsModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
